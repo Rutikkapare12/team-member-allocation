33954ca5e232d7d3a98b172bf716b487d0b3e83b
Rutik kapare <rutikkapare6340@gmail.com>
2023-06-13 23:42:58 +0530

    team-member-allocation project in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Employees from "./components/Employees";
+import GroupedTeamMembers from "./components/GroupedTeamMembers";
+import NoFound from "./components/NoFound";
+import { DataProvider } from "./context/DataContext";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <Router>
+        <Nav />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Employees />}></Route>
+          <Route
+            path="/GroupedTeamMembers"
+            element={<GroupedTeamMembers />}
+          ></Route>
+          <Route path="*" element={<NoFound />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+    </DataProvider>
   );
 }
-
-export default App;
---
+++ src/components/TeamMemberCard.jsx
@@ -0,0 +1,36 @@
+import femaleProfile from "../images/femaleProfile.jpg";
+import maleProfile from "../images/maleProfile.jpg";
+import { useContext } from "react";
+import DataContext from '../context/DataContext';
+const TeamMemberCard = ({ employee }) => {
+
+  const { handleEmployeeCardClick, selectedTeam } = useContext(DataContext);
+
+  return (
+    <div
+      key={employee.id}
+      id={employee.id}
+      className={
+        employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2"
+      }
+      style={{ cursor: "pointer" }}
+      onClick={handleEmployeeCardClick}
+    >
+      {employee.gender === "male" ? (
+        <img src={maleProfile} alt="" className="card-img-top" />
+      ) : (
+        <img src={femaleProfile} alt="" className="card-img-top" />
+      )}
+
+      <div className="card-body">
+        <h5 className="card-title">Full Name: {employee.fullName}</h5>
+        <p className="cart-text">
+          <b>Disignation :</b>
+          {employee.designation}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMemberCard;
